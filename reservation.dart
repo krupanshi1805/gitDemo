bed7d458c3084213d7be47fc5bdc289919d1a8f4
krupanshi1805 <kssohaliya@gmail.com>
2024-05-12 07:44:43 -0700

    - Add Reservation Screen

---
+++ reservation.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class Reservation extends StatefulWidget {
+  const Reservation({super.key});
+
+  @override
+  State<Reservation> createState() => _ReservationState();
+}
+
+class _ReservationState extends State<Reservation> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Reservation"),
+      ),
+    );
+  }
+}
